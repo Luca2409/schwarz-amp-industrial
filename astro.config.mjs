2628604401deaf09d8d4b3d70490056f771940fb
Luca2409 <luca.oppold@outlook.de>
2026-08-27 22:08:45 +0200

    deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
     site: 'https://luca2409.github.io',
-    base: '/luca-schwarz-personal',
+    base: '/schwarz-amp-industrial',
 });
